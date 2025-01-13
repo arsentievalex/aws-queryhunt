a4d0f6f686d7d8426654c27a24a9c8d8806934da
Oleksandr Arsentiev <108185255+arsentievalex@users.noreply.github.com>
2025-01-13 17:38:57 +0100

    Update home.py

--- home.py
+++ home.py
@@ -2,9 +2,9 @@ import streamlit as st
 from streamlit.components.v1 import html
 
 # read text from index.txt
-# with open('index.html', 'r') as file:
-#     html_content = file.read()
+with open('index.html', 'r') as file:
+    html_content = file.read()
 
-# html(html_content, height=600)
+html(html_content, height=600)
 
 
